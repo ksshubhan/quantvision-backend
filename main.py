0d3503ed613a6955b46027715a45111342a699f2
Sshubhan Kammari <sshubhan.kammari@student.manchester.ac.uk>
2025-10-23 00:07:36 +0100

    Fix: allow Vercel domains in CORS

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://quantvision.vercel.app",
+    "https://quantvision.vercel.app"
+    "https://quantvision-sshubhans-projects.vercel.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
