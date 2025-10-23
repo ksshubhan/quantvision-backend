4dc4ae3efd92e0ee2d4fef1cc9bbe88668c12ee1
Sshubhan Kammari <sshubhan.kammari@student.manchester.ac.uk>
2025-10-23 11:35:44 +0100

    fixed comma issue with URLS

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://quantvision.vercel.app"
+    "https://quantvision.vercel.app",
     "https://quantvision-sshubhans-projects.vercel.app",
 ]
 
